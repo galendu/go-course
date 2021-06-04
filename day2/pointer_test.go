9c6ba6a4b79c0e7c8cef6c3873caa0f9ab7860b4
yumaojun03 <18108053819@163.com>
2021-06-04 22:11:44 +0800

    指针

--- day2/pointer_test.go
+++ day2/pointer_test.go
@@ -9,3 +9,14 @@ func TestPointerRaw(t *testing.T) {
 	a := "string a"
 	fmt.Println(&a)
 }
+
+func TestPointer1(t *testing.T) {
+	var a *int
+	fmt.Println(a)
+}
+
+func TestPointer2(t *testing.T) {
+	var a *int = new(int)
+	fmt.Println(a)
+	fmt.Println(*a)
+}
