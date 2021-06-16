37d96f9c67c1e724a8025da04249d7c331120beb
yumaojun03 <18108053819@163.com>
2021-06-16 21:11:50 +0800

    补充指针

--- day3/slice_test.go
+++ day3/slice_test.go
@@ -11,3 +11,13 @@ func TestSliceSize(t *testing.T) {
 	fmt.Println(a[2])
 	fmt.Println(a, len(a), cap(a))
 }
+
+func TestSlice(t *testing.T) {
+	hlist := make(map[string][]string)
+
+	h1key := []string{}
+	h1key = append(h1key, "1", "2", "3")
+	hlist["1"] = h1key
+
+	fmt.Println(hlist)
+}
