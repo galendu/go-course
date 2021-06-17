eec96e35ad9d0e15b2dc9312ac64c34c17dd1560
yumaojun03 <18108053819@163.com>
2021-06-17 23:58:59 +0800

    添加字符串

---
+++ day3/strings_test.go
@@ -0,0 +1,29 @@
+package day3
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func TestStrings1(t *testing.T) {
+	a := "hello"
+	fmt.Println([]byte(a))
+}
+
+func TestStrings2(t *testing.T) {
+	a := "hello"
+	b := []byte(a)
+	b[0] = 'x'
+	fmt.Println(string(b))
+}
+
+func TestStrings3(t *testing.T) {
+	a := "hello"
+	fmt.Println(len(a), a[0], a[1:3])
+}
+
+func TestString4(t *testing.T) {
+	fmt.Println(strings.Compare("ab", "cd"))
+	fmt.Println(strings.EqualFold("ab", "AB"))
+}
